7344a46eec3e33d7090f18c9a21e995aa5c1af7e
Olsm <OlavOlsm@gmail.com>
2016-11-26 23:31:49 +0100

    get filenames in directory and add CmakeLists

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
+#include <dirent.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(void) {
+    DIR *dir;
+    struct dirent *dirEntry;
+    char files[32][32];
+    int countFiles = 0;
 
+    dir = opendir("mickey");
+    if (!dir) {
+        perror ("could not read directory");
+        return -1;
+    }
+
+    while ((dirEntry = readdir(dir)) != NULL) {
+        if (dirEntry->d_type == DT_REG) {
+            strcpy(files[countFiles], dirEntry->d_name);
+            countFiles++;
+        }
+    }
+    closedir(dir);
+
+    for (int i = 0; i < countFiles; ++i) {
+        printf("%s \n", files[i]);
+    }
 }
