7cdb365e20693b9d5355e35e5d2828228a0cb1f4
Olsm <OlavOlsm@gmail.com>
2016-11-27 01:14:39 +0100

    merge 30x30 chars from text files and output to new text file

--- main.c
+++ main.c
@@ -16,13 +16,43 @@ int main(void) {
 
     while ((dirEntry = readdir(dir)) != NULL) {
         if (dirEntry->d_type == DT_REG) {
-            strcpy(files[countFiles], dirEntry->d_name);
+            strcpy(files[countFiles], "mickey/");
+            strcat(files[countFiles], dirEntry->d_name);
             countFiles++;
         }
     }
     closedir(dir);
 
+    char line[sizeof(char)*30];
+    char lines[2048][sizeof(line)];
+    int count = 0;
+    FILE * file = NULL;
+
     for (int i = 0; i < countFiles; ++i) {
-        printf("%s \n", files[i]);
+        // Try and open the file for reading, or exit with error
+        file = fopen(files[i], "r");
+        if (file == NULL){
+            perror("Could not open file");
+            return -1;
+        }
+
+        // Read each line and add them to array of lines
+        while (fgets(line, sizeof(char)*30, file)) {
+            strcpy(lines[count], line);
+            count++;
+        }
+
+        fclose(file);
     }
+
+
+    FILE *fp;
+    fp = fopen("mickey/mickey.txt", "w");
+
+    int i;
+    for (i = 0; i < count; i++) {
+        fprintf(fp, lines[i]);
+    }
+
+    fclose(fp);
 }
